77e117a0fd438fd9cfb495f81833a21e13303c0e
Replit user <>
2023-02-13 16:59:30 +0300

    Start commit

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils.timezone import localtime, now
+from datetime import timedelta
+import re
 
 
 class Passcard(models.Model):
@@ -7,6 +10,7 @@ class Passcard(models.Model):
     passcode = models.CharField(max_length=200, unique=True)
     owner_name = models.CharField(max_length=255)
 
+    
     def __str__(self):
         if self.is_active:
             return self.owner_name
@@ -19,6 +23,15 @@ class Visit(models.Model):
     entered_at = models.DateTimeField()
     leaved_at = models.DateTimeField(null=True)
 
+    
+    def is_long(self, minutes=60):
+        return self.get_duration() > timedelta(minutes=minutes)
+
+    
+    def get_duration(self):
+        return localtime(self.leaved_at) - localtime(self.entered_at)
+        
+       
     def __str__(self):
         return '{user} entered at {entered} {leaved}'.format(
             user=self.passcard.owner_name,
@@ -28,3 +41,13 @@ class Visit(models.Model):
                 if self.leaved_at else 'not leaved'
             )
         )
+
+
+def format_duration(duration): 
+    duration_stamp = (str(duration).split(".")[0]).split(":")
+    if duration < timedelta(days=1):
+        return f'{duration_stamp[0]}ч {duration_stamp[1]}мин'
+    else:
+        duration_stamp = re.split(' day, |:',str(duration))
+        return f'{duration_stamp[0]}дн {duration_stamp[1]}ч {duration_stamp[2]}мин'
+       
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,22 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
+from datacenter.models import format_duration
 from django.shortcuts import render
+from django.utils.timezone import localtime
 
 
 def storage_information_view(request):
-    # Программируем здесь
+    non_closed_visits = []
+    for visit in Visit.objects.filter(leaved_at__isnull=True):        
+        non_closed_visits.append({
+            'who_entered': visit.passcard,
+            'entered_at': localtime(visit.entered_at),
+            'duration': format_duration(visit.get_duration()),
+            'is_strange': visit.is_long(),
+        })    
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
+    
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
