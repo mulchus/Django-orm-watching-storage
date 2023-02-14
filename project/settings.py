7259e5c3c42f6a0127a5dcefb6e437028ab09f03
Replit user <>
2023-02-14 17:25:16 +0300

    Added dj_database_url - dont work

--- project/settings.py
+++ project/settings.py
@@ -1,21 +1,37 @@
 import os
 from environs import Env
+import dj_database_url
 
 
 env = Env()
 env.read_env()
 
 SECRET_KEY = env('SECRET_KEY')
+
+db_config = env('db_config')
+
 DATABASES = {
-    'default': {
-        'ENGINE': env('ENGINE'),
-        'HOST': env('HOST'),
-        'PORT': env('PORT'),
-        'NAME': env('NAME'),
-        'USER': env('USER'),
-        'PASSWORD': env('PASSWORD'),
-    }
+    'default': db_config,
 }
+
+
+# DB_ENGINE = 'django.db.backends.postgresql_psycopg2'
+# DB_HOST = 'checkpoint.devman.org'
+# DB_PORT = '5434'
+# DB_NAME = 'checkpoint'
+# DB_USER = 'guard'
+# DB_PASSWORD = 'osim5'
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': env('ENGINE'),
+#         'HOST': env('HOST'),
+#         'PORT': env('PORT'),
+#         'NAME': env('NAME'),
+#         'USER': env('USER'),
+#         'PASSWORD': env('PASSWORD'),
+#     }
+# }
 
 INSTALLED_APPS = ['datacenter']
 
